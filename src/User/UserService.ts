5c61d1090482eeb4ec17117918528fd436645def
Jeff Costa <jeffersomcst2@gmail.com>
2022-07-23 12:53:39 -0300

    adicao do create user

--- src/User/UserService.ts
+++ src/User/UserService.ts
@@ -3,8 +3,16 @@ import User from "./User";
 
 export default class UserService {
   //Create function get user
-  async getUser() {
+  async getAll() {
     const user = await AppDataSource.manager.find(User);
     return user;
+  }
+  async getOne(id: number): Promise<User|null> {
+    const user:User | null = await AppDataSource.manager.findOneBy(User, { id: id })
+    return user;
+  }
+  async create(user: User): Promise<User> {
+    const createdUser = await AppDataSource.manager.save(User, user);
+    return createdUser;
   }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,20 +1,36 @@
 import Rifa from "./rifa/Rifa";
+import User from "./User/User";
 import UserService from "./User/UserService";
 
 const express = require("express");
 const app = express();
+app.use(express.json()) 
 
-app.get("/rifas", function (req:any, res:any) {
-    const rifa = new Rifa(10, 1000, 200);
-  
-    rifa.status = true;
-    res.send(rifa.status);
-  });
-  app.get('/users', async function (req:any, res:any) { 
-     const userService  = new UserService();
-     const users = await userService.getUser();
-     
-     res.send(users);
-  })
+app.get("/rifas", function (req: any, res: any) {
+  const rifa = new Rifa(10, 1000, 200);
+
+  rifa.status = true;
+  res.send(rifa.status);
+});
+app.get("/users", async function (req: any, res: any) {
+  const userService = new UserService();
+  const users: User[] = await userService.getAll();
+
+  res.send(users);
+});
+app.get("/users/:id", async function (req: any, res: any) {
+  console.log(req.params.id);
+  const userService = new UserService();
+  const users: User | null = await userService.getOne(req.params.id);
+  res.send(users);
+});
+
+//Create route express create user 
+app.post("/users", async function (req: any, res: any) {
+  const userService = new UserService();
+  const user = new User(req.body.name , req.body.email, req.body.password);
+  const createdUser = await userService.create(user);
+  res.send(createdUser);
+});
 
 export default app;
